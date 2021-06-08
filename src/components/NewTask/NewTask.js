3b507a283108af3ce051defedea30d067f7edbc9
juliodiaz <julio@vimbly.com>
2021-06-07 23:14:55 -0500

    Use a post request and bind

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,41 +1,31 @@
-import { useState } from 'react';
+import { React } from 'react';
 
 import Section from '../UI/Section';
 import TaskForm from './TaskForm';
+import useHttp from '../../hooks/use-http';
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const {isLoading, error, sendRequest: sendTaskRequest} = useHttp();
+
+  function createdTask(taskText, taskData){
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  }
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        'https://react-http-154f5-default-rtdb.firebaseio.com/tasks.json',
-        {
-          method: 'POST',
-          body: JSON.stringify({ text: taskText }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
+      sendTaskRequest({
+        url: 'https://react-http-154f5-default-rtdb.firebaseio.com/tasks.json',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: {text: taskText}
+      }, createdTask.bind(null, taskText) // bind method allows to modify the current definition of a funtion
+      // the first parameter null in this case, indicates the funcion scope, otherwise is the global scope.
+      // the following args are added automatically
       );
-
-      if (!response.ok) {
-        throw new Error('Request failed!');
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || 'Something went wrong!');
-    }
-    setIsLoading(false);
   };
 
   return (
